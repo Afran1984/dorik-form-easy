c8341971b80c9fe4f10a01bcde9b31b471e85f65
Afran1984 <afranrazzak1984@gmail.com>
2025-09-15 22:05:43 +0600

    add Navbar & Make a Components

---
+++ components/Topbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className="w-full bg-gray-800 text-white flex justify-between items-center px-6 py-3 shadow-md">
+      <h1 className="text-lg font-bold">Dynamic Form Builder</h1>
+    </div>
+      
+  )
+}
+
+export default Topbar
